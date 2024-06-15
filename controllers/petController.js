49669946a1d4790b3cf213599a91e316d9e0c758
zeeshan <xeeshanb@gmail.com>
2024-06-15 08:46:30 +0500

    Finished contact form feature.

--- controllers/petController.js
+++ controllers/petController.js
@@ -77,7 +77,7 @@ exports.actuallyUpdatePet = async (req, res) => {
   if (typeof req.body.name != "string") {
     req.body.name = ""
   }
-  
+
   if (typeof req.body.description != "string") {
     req.body.description = ""
   }
@@ -112,11 +112,11 @@ exports.storePet = async (req, res) => {
   if (typeof req.body.name != "string") {
     req.body.name = ""
   }
-  
+
   if (typeof req.body.description != "string") {
     req.body.description = ""
   }
-  
+
   let ourObject = { name: sanitizeHtml(req.body.name, sanitizeOptions), birthYear: new Date().getFullYear(), species: sanitizeHtml(req.body.species, sanitizeOptions), description: sanitizeHtml(req.body.description, sanitizeOptions) }
 
   if (req.body.birthYear > 999 && req.body.birthYear < 9999) {
